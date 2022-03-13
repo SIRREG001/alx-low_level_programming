38a08ec3c1898d77439c237fafcc7ef9764d6ef2
SIRREG001 <udochukwureginald78@gmail.com>
2022-03-13 15:58:12 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,15 +14,15 @@ int main(void)
 
 	while (i < 10)
 	{
-	        putchar(i + '0');
-	if (i < 9)
+		putchar(i + '0');
+		if (i < 9)
 		{
-	                putchar(44);
-	                putchar(32);
+			putchar(44);
+			putchar(32);
 		}
-	i++;
+		i++;
 	}
 	putchar('\n');
 
-        return (0);
+	return (0);
 }
