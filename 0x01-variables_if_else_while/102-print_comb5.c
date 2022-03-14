422fd8183a32b0af247912f1262c5adf00af0aea
SIRREG001 <udochukwureginald78@gmail.com>
2022-03-13 17:05:11 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
-
 /**
-
  * main - main block
-
  * Description: Print all possible combination of two two-digit numbers.
-
  * Numbers should range from 0 to 99
-
  * The two numbers should be separated by a space.
-
  * All numbers should be printed wih two digits. 1 should be printed as 01
-
  * Combination of numbers must be separated by comma followed by a space.
-
  * Combinations of numbers should be printed in ascending order.
-
  * `00 01` and `01 00`are considered as the same combination.
-
  * You can only use `putchar` to print to console.
-
  * You can only use 'putchar` up to 8 times.
-
  * You are not allowed to use any variable of type `char`
-
  * Return: 0
-
  */
 int main(void)
 {
