15590dada3eac32f5d564d4885aa66782b51f3d7
SIRREG001 <udochukwureginald78@gmail.com>
2022-03-16 17:30:27 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  main - print _putchar followed by a new line
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *ch = "_putchar";
+
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
