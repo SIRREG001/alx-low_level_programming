cb32b805d6fd4169cda338f1ef597d7736a42494
SIRREG001 <udochukwureginald78@gmail.com>
2022-03-16 21:44:08 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,27 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
-		while (n < 98)
+		for (; n <= 98; n++)
 		{
-			printf("%d, ", n);
-			n--;
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
 		}
+		printf("\n");
 	}
-	printf("98\n");
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
 }
