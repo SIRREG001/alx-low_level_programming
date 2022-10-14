191255051029dd945b4d1d9253b3d5a2fd06d58f
SIRREG001 <udochukwureginald78@gmail.com>
2022-10-13 19:53:50 -0700

    Print characters with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *Print a set of characters with puts()
- *main -entry point
- *return 0 after printing function
+ * main - entry point
+ *
+ * Return: 0 after printing function
  */
 int main(void)
 {
-	char my_string[] = "\"Programming is like building a multilingual puzzle";
-	puts(my_string);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
