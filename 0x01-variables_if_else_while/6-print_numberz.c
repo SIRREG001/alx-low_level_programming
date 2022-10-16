42ee3f93729d15afccd4fcbc30fc13d16132c48b
SIRREG001 <udochukwureginald78@gmail.com>
2022-10-16 16:03:59 -0700

    use putchar to print integer decimal numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 ends program
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 48; numbers <= 57; numbers++)
+	{
+		putchar(numbers);
+	}
+	putchar('\n');
+	return (0);
+}
