6a03f2222e4721a02652558a43d48d5718deb85f
SIRREG001 <udochukwureginald78@gmail.com>
2022-10-23 18:53:05 -0700

    check if a character is a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - entry point
+ * Return: sign depending if number is greater than 0 less than 0 or equal to 0
+ * @n: number to be compared
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar('1');
+		_putchar(',');
+		return (-1);
+	}
+}
+
