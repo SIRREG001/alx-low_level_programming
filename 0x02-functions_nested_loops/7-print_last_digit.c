f8038a8e319dbac179a310b60a056d2544d0a586
SIRREG001 <udochukwureginald78@gmail.com>
2022-10-23 19:33:54 -0700

    Return the last digit of any mumber

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ * @n: integer inwhich its last digit is to be found
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	if (n > 0)
+	{
+		last = n % 10;
+		_putchar(last + '0');
+		return (last);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		last = (-1 * (n % 10));
+		_putchar(last + '0');
+		return (last);
+	}
+}
