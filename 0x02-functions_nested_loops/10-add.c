8aab0e0b8f1b4f20f72a7df793326a3c7d1a4cb0
SIRREG001 <udochukwureginald78@gmail.com>
2022-10-25 17:14:35 -0700

    function that adds two numbers and return their sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - entry point
+ * @a: first integer
+ * @b: second integer
+ * Return: sum which is the result
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
