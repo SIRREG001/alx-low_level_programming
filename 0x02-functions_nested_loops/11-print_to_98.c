49ac645f5a9e2ccc9bc3a6ad5bc05c990cd05492
SIRREG001 <udochukwureginald78@gmail.com>
2022-10-27 16:21:26 -0700

    Print all natural numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,18 +8,30 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n = n; n < 98; n++)
+		for (n = n; n <= 98; n++)
 		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
 			printf("%d, ", n);
-			printf("%d\n", 98);
 		}
+	}
+	else if (n == 98)
+	{
+		printf("%d\n", n);
 	}
 	else
 	{
-		for (n = n; n > 98; n--)
+		for (n = n; n >= 98; n--)
 		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
 			printf("%d, ", n);
-			printf("%d\n", 98);
 		}
 	}
 }
