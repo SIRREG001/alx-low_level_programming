e53e0008c8e7b2c01e92166537c70c6cbb88496b
SIRREG001 <udochukwureginald78@gmail.com>
2022-10-28 15:44:22 -0700

    print n-times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,39 +11,36 @@ void print_times_table(int n)
 	int b;
 	int c;
 
-	for (a = 0; a <= n; a++)
+	if (n <= 15 && n >= 0)
 	{
-		for (b = 0; b <= n; b++)
+		for (a = 0; a <= n; a++)
 		{
-			c = a * b;
-			if ((c / 10) == 0)
+			_putchar('0');
+			for (b = 0; b <= n; b++)
 			{
-				if (b == 0)
+				_putchar(',');
+				_putchar(' ');
+				c = a * b;
+				if (c <= 99)
 				{
-					_putchar ('0');
+					_putchar(' ');
 				}
-				if (b != 0)
+				if (c <= 9)
 				{
-					_putchar (' ');
-					_putchar ((c % 10) + '0');
+					_putchar(' ');
 				}
-				if (b < n)
+				if (c <= 99 && c >= 10)
 				{
-					_putchar(',');
-					_putchar (' ');
+					_putchar((c / 10) + '0');
 				}
+				if (c > 99)
+				{
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+				}
+				_putchar((c % 10) + '0');
 			}
-			else
-			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b < n)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
-			}
+			_putchar('\n');
 		}
-		_putchar ('\n');
 	}
 }
