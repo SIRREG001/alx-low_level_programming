cd79fada9f831ee852ad92da15e1622f9ac05fe0
SIRREG001 <udochukwureginald78@gmail.com>
2022-11-01 05:45:44 -0700

    test for a digit value

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - entry point
+ * @c: character to be tested for digit value
+ * Return: 1 or 0 if character is a digit and otherwise respectively
+ */
+int _is digit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
