292a73822384d50fa6cfdd65690a751df7129da0
SIRREG001 <udochukwureginald78@gmail.com>
2022-11-01 05:57:34 -0700

    check if a character is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,7 +4,7 @@
  * @c: character to be tested for digit value
  * Return: 1 or 0 if character is a digit and otherwise respectively
  */
-int _is digit(int c)
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 	{
