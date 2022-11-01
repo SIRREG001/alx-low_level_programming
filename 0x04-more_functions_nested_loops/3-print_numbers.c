0a835808c2948b789cfca2ec6a3ff1930ee2909f
SIRREG001 <udochukwureginald78@gmail.com>
2022-11-01 06:31:12 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,19 @@
 #include "main.h"
+
 /**
- * print_numbers - entry point
+ * print_numbers - print  0 - 9
+ *
+ * Return: void
  */
+
 void print_numbers(void)
 {
-	char c;
+	char a = 0;
 
-	for (c = 0; c <= 9; c++)
+	while (a <= 9)
 	{
-		_putchar(c + '0');
+		_putchar(a + '0');
+		a++;
 	}
 	_putchar('\n');
 }
