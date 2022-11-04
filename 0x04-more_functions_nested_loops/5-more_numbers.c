5ea6d92daad843eaedeaf3d509979327d7ed55c0
SIRREG001 <udochukwureginald78@gmail.com>
2022-11-04 09:03:36 -0700

    Print numbers from 0 to 14 ten times using putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - entry point
+ */
+void more_numbers(void)
+{
+	int a, i;
+	int b = 0;
+
+	while (b <= 10)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			i = a;
+			if (a > 9)
+			{
+				_putchar('1');
+				i = a % 10;
+			}
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+		b += 1;
+	}
+	_putchar('\n');
+}
